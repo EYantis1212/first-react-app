d68a744c7f824e3c6be337e51df79ca81e13047d
yantis <yantis1212@outlook.com>
2022-02-05 10:00:37 -0700

    feat
    Add error handling

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,14 +5,28 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { lat: null };
+		// This is the only time we do direct assignment...else we use setState
+		this.state = { lat: null, errorMessage: '' };
+
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({ lat: position.coords.latitude });
+			},
+			(err) => {
+				this.setState({ errorMessage: err.message });
+			}
+		);
 	}
 	render() {
-		window.navigator.geolocation.getCurrentPosition(
-			(position) => console.log(position),
-			(err) => console.log(err)
-		);
-		return <div>Latitude: </div>;
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <div>Latitude: {this.state.lat}</div>;
+		}
+
+		return <div>Loading...</div>;
 	}
 }
 
