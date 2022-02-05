3ff21c1e04968d7a043fbd262826e3912e93dd30
yantis <yantis1212@outlook.com>
2022-02-05 10:32:34 -0700

    feat
    Add season logic to SeasonDisplay.js

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		// This is the only time we do direct assignment...else we use setState
-		this.state = { lat: null, errorMessage: '' };
-
+	state = {
+		lat: null,
+		errorMessage: '',
+	};
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => {
-				this.setState({ lat: position.coords.latitude });
-			},
-			(err) => {
-				this.setState({ errorMessage: err.message });
-			}
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
 		);
 	}
 	render() {
@@ -23,7 +19,7 @@ class App extends React.Component {
 		}
 
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
 		return <div>Loading...</div>;
