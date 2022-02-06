4c8f22a5f6a7872e23f67f1b2b4d18c1ccb938ab
yantis <yantis1212@outlook.com>
2022-02-06 11:38:07 -0700

    Add: (feat) Add click event callback
    Using prop system to get selected video from Item to app state

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -6,6 +6,7 @@ import VideoList from './VideoList';
 class App extends React.Component {
 	state = {
 		videos: [],
+		selectedVideo: null,
 	};
 
 	onTermSubmit = async (term) => {
@@ -16,12 +17,18 @@ class App extends React.Component {
 		});
 		this.setState({ videos: response.data.items });
 	};
+	onVideoSelect = (video) => {
+		console.log('from the app', video);
+	};
 
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
-				<VideoList videos={this.state.videos} />
+				<VideoList
+					onVideoSelect={this.onVideoSelect}
+					videos={this.state.videos}
+				/>
 			</div>
 		);
 	}
